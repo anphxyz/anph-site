2d5e5963f9dee01e2eff8249b8f26d080fdee6e0
An Ph <aanltll2992@gmail.com>
2022-02-09 11:24:54 +0700

    Update redisHelper.js

--- utils/redisHelper.js
+++ utils/redisHelper.js
@@ -6,9 +6,13 @@
 const aredis = require('aredis-xyz')
 
 exports.build = (hash) => {
-    const opt = { port: process.env.REDIS_PORT }
     // expired after 3 days (auto remove)
-    const instance = aredis.build({ ...opt, prefix: 'ANPH-XYZ-', hashName: hash, expire: 3 * 24 * 60 * 60 });
+    const instance = aredis.build({
+        port: process.env.REDIS_PORT,
+        prefix: 'ANPH-XYZ-',
+        hashName: hash,
+        expire: 3 * 24 * 60 * 60
+    });
 
     log.warning('>>build', instance.hashName);
     return instance;
